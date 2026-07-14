2927098abd4eb9cd59339e6a6f7730bc7bdff9c7
Ömer Apaydın <omerapaydin90@gmail.com>
2026-07-15 01:18:25 +0300

    Initial Commit

---
+++ ByRoot/ByRootApp.swift
@@ -0,0 +1,17 @@
+//
+//  ByRootApp.swift
+//  ByRoot
+//
+//  Created by Ömer Apaydın on 15.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct ByRootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
